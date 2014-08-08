c1a2c39a581fefa4062d63d567db9a7ec78e85c7
drPepper <drpepper240@users.noreply.github.com>
2014-08-08 17:57:29 +0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -951,8 +951,16 @@ function DrawWidgetLaserEm(guiDataId)
 	local selected = nil
 	if T_guiTempData[widget.tTGuiId] ~= nil then
 		selected = T_guiTempData[widget.tTGuiId]
-	else
-		PrintDbg("DrawWidgetLaserEm(): TT tempdata missing",1)
+	end
+	
+	--TODO remove debug
+	if T_guiTempData[widget.tTGuiId].targetList == nil then
+		PrintDbg("DrawWidgetLaserEm(): TT targetList missing", 1)
+	elseif T_guiTempData[widget.tTGuiId].targetList[selected] == nil then
+		PrintDbg("DrawWidgetLaserEm(): TT selection missing", 1)
+	end
+	
+
 	
 	local canFire, r, t, p = false, nil, nil, nil 
 	
